6895dbde090d2556a24a78aa09386db0c57a19fc
Sebastian Zeleznik <sajgony@gmail.com>
2016-11-11 16:41:40 -0700

    In the middle of third lesson

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; //Forms module allow me to use NgModel for two way data binding
+
 import { AppComponent }   from './app.component';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
